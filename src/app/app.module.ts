8e021a00d1db6c8a29f6796a4ed32d8eb4641582
zainab <zainabtarada3@gmail.com>
2024-05-16 17:45:43 +0300

    this is supposed to be the initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyBDQ0RDO0hSOA20er-BvLnwY6heShXlZQ8",
+  authDomain: "project-f1277.firebaseapp.com",
+  projectId: "project-f1277",
+  storageBucket: "project-f1277.appspot.com",
+  messagingSenderId: "289110829389",
+  appId: "1:289110829389:web:0333534fae0de00fa7ef04",
+  measurementId: "G-5CH78EE3FL"
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(firebaseConfig)), provideFirestore(() => getFirestore()), provideAuth(() => getAuth()) ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
